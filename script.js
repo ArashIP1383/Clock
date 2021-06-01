14a2d82e3a7661f0e2e38479114bd7cbacdbc91b
ArashIP <arashmahmoodi665@gmail.com>
2021-06-01 11:01:32 +0430

    First

---
+++ script.js
@@ -0,0 +1,22 @@
+const hours = document.querySelector('#h')
+const mins = document.querySelector('#m')
+const seconds = document.querySelector('#sec')
+
+function time(){
+    var d = new Date();
+    const hour = d.getHours()
+    const min = d.getMinutes()
+    let second = d.getSeconds()
+    second = timechecker(second)
+    hours.style.transform = `rotate(${hour * 30}deg)`
+    mins.style.transform = `rotate(${min * 6}deg)`
+    seconds.style.transform = `rotate(${second * 6}deg)`
+}
+
+setInterval(time,1000)
+
+
+function timechecker(second){
+    if(second < 10){ second = "0"+second}
+    return second
+}
